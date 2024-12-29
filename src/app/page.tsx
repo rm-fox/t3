e46322eb7bac5821441de3b67c1bc5bed1bc3e62
Fox <fox-f@outlook.com>
2024-12-29 09:49:40 +0100

    whitepaper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,6 +47,12 @@ const MainPage = () => {
         >
           [docs]
         </button>
+        <button
+          onClick={() => window.open("/whitepaper.pdf", "_blank")}
+          className="px-4 py-2 bg-blue-600 text-white rounded-md w-full sm:w-auto"
+        >
+          [whitepaper]
+        </button>
       </div>
     </div>
   );
